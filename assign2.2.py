edc523b67f9ceda01f59986d55e955e9df1423cc
SuhaniMH11 <suhanihedau123@gmail.com>
2026-01-28 20:52:30 +0530

    starter code

---
+++ assign2.2.py
@@ -0,0 +1,22 @@
+h = float(input("Enter hardness: "))
+c = float(input("Enter carbon content: "))
+t = float(input("Enter tensile strength: "))
+
+c1 = h > 50
+c2 = c < 0.7
+c3 = t > 5600
+
+if c1 and c2 and c3:
+    grade = 10
+elif c1 and c2:
+    grade = 9
+elif c2 and c3:
+    grade = 8
+elif c1 and c3:
+    grade = 7
+elif c1 or c2 or c3:
+    grade = 6
+else:
+    grade = 5
+
+print("Grade:", grade)
